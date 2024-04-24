31f05e40112ca9d3ad4ba235b01671136afaeded
Sri Lakshmi Polavarapu <srilakshmipolavarapu@Sris-MacBook-Air.local>
2024-04-23 17:46:46 -0700

    my portfolio

---
+++ js/hashremove.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+  const menuBtn = $(".hashremove");
+  menuBtn.click(() => {
+    setTimeout(() => {
+      removeHash();
+    }, 1);
+  });
+  function removeHash() {
+    history.replaceState(
+      "",
+      document.title,
+      window.location.origin + window.location.pathname + window.location.search
+    );
+  }
+});
---
+++ js/load.js
@@ -0,0 +1,17 @@
+var projs = [];
+var links = [
+  "https://github.com/SriLakshmiPolavarapu/CV_Color-Transfer.git",
+  "https://github.com/SriLakshmiPolavarapu/Image-Captioning.git",
+  "https://github.com/SriLakshmiPolavarapu/Internet-Relay-Chat.git",
+  "https://github.com/SriLakshmiPolavarapu/Snake-and-Ladder.git",
+  "https://github.com/SriLakshmiPolavarapu/Hotel_Management_System.git",
+  "https://github.com/SriLakshmiPolavarapu/Analysis-of-Indian-Premier-League-Data.git",
+  "https://github.com/SriLakshmiPolavarapu/Ping-Pong-Game---Unity.git",
+  "https://github.com/SriLakshmiPolavarapu/Angry-Bird-Game---Unity.git",
+  "https://github.com/SriLakshmiPolavarapu/Predator-Clash-Game---Unity.git",
+  "https://github.com/SriLakshmiPolavarapu/CreditCardFraudDetection.git"
+];
+for (i = 0; i < links.length; i++) {
+  projs.push(new Image());
+  projs[i].src = links[i];
+}
